9e607be73eef8d3ea6517eb18622107321b86e3f
creesch <creesch.r@gmail.com>
2015-01-03 19:58:56 +0100

    version numbers. Even safari is a "thing"

--- tbutils.js
+++ tbutils.js
@@ -34,8 +34,8 @@ function initwrapper() {
         ECHO = 'echo', TB_KEY = 'Toolbox.';
 
     // Public variables
-    TBUtils.toolboxVersion = '3.0.4' + ((betaRelease) ? ' (beta)' : '');
-    TBUtils.shortVersion = 304; //don't forget to change this one!  This is used for the 'new version' notification.
+    TBUtils.toolboxVersion = '3.0.5' + ((betaRelease) ? ' (beta)' : '');
+    TBUtils.shortVersion = 305; //don't forget to change this one!  This is used for the 'new version' notification.
     TBUtils.releaseName = 'Illuminati Ibis';
     TBUtils.configSchema = 1;
     TBUtils.notesSchema = 4;
