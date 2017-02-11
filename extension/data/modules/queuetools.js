b5023cefb3340a3495c1c3f0d28d912a722db0ff
Geo1088 <georgej1088@gmail.com>
2017-02-11 14:06:31 -0500

    ...right, we use semicolons here

--- extension/data/modules/queuetools.js
+++ extension/data/modules/queuetools.js
@@ -103,7 +103,7 @@ self.register_setting('groupCommentsOnModPage', {
     'default': false,
     'beta': 'true',
     'title': 'Group comments by their parent submission when viewing mod listings.'
-})
+});
 
 // A better way to use another module's settings.
 self.register_setting('subredditColorSalt', {
@@ -943,26 +943,26 @@ self.init = function () {
         // TODO: fix that
         function groupThings () {
             var threadGroups = {},
-                threadIDs = [] // because who needs Object.keys() anyway
+                threadIDs = []; // because who needs Object.keys() anyway
 
             // Save a copy of each link/comment and record its parent thread ID
             $('.sitetable .thing').each(function () {
-                var $thing = $(this)
-                var threadID
+                var $thing = $(this),
+                    threadID;
                 if ($thing.hasClass('comment')) {
                     // Find ID of the parent submission from title URL
-                    threadID = $thing.find('.parent a.title').attr('href').match(/\/comments\/([a-z0-9]+)\//)[1]
+                    threadID = $thing.find('.parent a.title').attr('href').match(/\/comments\/([a-z0-9]+)\//)[1];
                 } else {
                     // I am the parent submission, so get my own ID
                     // TBUtils.getThingInfo() is overkill here
-                    threadID = $thing.attr('data-fullname').replace('t3_', '')
+                    threadID = $thing.attr('data-fullname').replace('t3_', '');
                 }
                 // Only record thread IDs once each
-                if (threadIDs.indexOf(threadID) < 0) threadIDs.push(threadID)
+                if (threadIDs.indexOf(threadID) < 0) threadIDs.push(threadID);
                 // Store the element itself so we can move it around later
-                if (threadGroups[threadID] == null) threadGroups[threadID] = []
-                threadGroups[threadID].push($thing)
-            })
+                if (threadGroups[threadID] == null) threadGroups[threadID] = [];
+                threadGroups[threadID].push($thing);
+            });
 
 
             // Create wrapper elements for each thread ID. Each wrapper will
@@ -972,20 +972,20 @@ self.init = function () {
             $.each(threadIDs, function (index, id) {
                 // Each wrapper will contain all the things associated with
                 // a single submission, including the submission itself
-                var $wrapper = $('<div>').addClass('tb-comment-group').attr('data-id', id)
-                $('#siteTable').append($wrapper)
+                var $wrapper = $('<div>').addClass('tb-comment-group').attr('data-id', id);
+                $('#siteTable').append($wrapper);
                 // Loop through each thing associated with the submission
                 $.each(threadGroups[id], function (index) {
                     // Add the thing to this wrapper
-                    threadGroups[id][index].appendTo($wrapper)
+                    threadGroups[id][index].appendTo($wrapper);
                 })
                 // Visual separation
-                $wrapper.append($('<hr />'))
-            })
+                $wrapper.append($('<hr />'));
+            });
         }
 
         if (TBUtils.isModpage && groupCommentsOnModPage) {
-            groupThings()
+            groupThings();
         }
 
     }
