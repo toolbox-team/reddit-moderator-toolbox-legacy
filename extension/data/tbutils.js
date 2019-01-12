d0e971580c621f954b5516410094305a86c84a03
creesch <creesch@users.noreply.github.com>
2019-01-12 22:44:28 +0100

    3.8.0 because debugging

--- extension/data/tbutils.js
+++ extension/data/tbutils.js
@@ -124,8 +124,8 @@ function initwrapper() {
 
 
         // Public variables
-        TBUtils.toolboxVersion = `3.7.4${(betaRelease) ? ' (beta)' : ''}`;
-        TBUtils.shortVersion = 374; //don't forget to change this one!  This is used for the 'new version' notification.
+        TBUtils.toolboxVersion = `3.8.0${(betaRelease) ? ' (beta)' : ''}`;
+        TBUtils.shortVersion = 380; //don't forget to change this one!  This is used for the 'new version' notification.
         TBUtils.releaseName = 'Cleaning Cockcroach';
         TBUtils.configSchema = 1;
         TBUtils.notesSchema = 6;
